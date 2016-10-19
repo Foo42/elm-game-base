4ae3f7b609bd00dd9463708fd8831e718ffd1334
Julian Haeger <julianhaeger@gmail.com>
2016-10-19 08:02:32 +0100

    making velocity etc into vectors

--- Game.elm
+++ Game.elm
@@ -22,18 +22,32 @@ main =
 
 -- MODEL
 
+type alias Vector2 = {x : Float, y: Float}
+
+addVecs : Vector2 -> Vector2 -> Vector2
+addVecs a b =
+    Vector2 (a.x + b.x) (a.y + b.y)
+
+scaleVec : Float -> Vector2 -> Vector2
+scaleVec scale vec =
+    Vector2 (vec.x * scale) (vec.y * scale)
+
+vectorMagnitude : Vector2 -> Float
+vectorMagnitude v =
+    sqrt (v.x^2 + v.y^2)
+
 type alias Physical t =
-    {t | accelleration : Float
-    , velocity : Float
-    , position : Float}
+    {t | accelleration : Vector2
+    , velocity : Vector2
+    , position : Vector2}
 
 type alias Player = Physical { shotsFired : Int }
 
 player : Player
 player =
-    { accelleration = 0
-    , velocity = 0
-    , position = 0
+    { accelleration = Vector2 0 0
+    , velocity = Vector2 0 0
+    , position = Vector2 0 0
     , shotsFired = 0
     }
 
@@ -79,10 +93,10 @@ keyDown keyCode model =
             {model | player = incrementShotsFired model.player}
 
         ArrowLeft ->
-            {model | player = updateAcceleration -1.0 model.player}
+            {model | player = updateAcceleration {x = -1.0, y= 0} model.player}
 
         ArrowRight ->
-            {model | player = updateAcceleration 1.0 model.player}
+            {model | player = updateAcceleration {x = 1.0, y= 0} model.player}
 
         _ ->
             model
@@ -92,13 +106,15 @@ keyUp : KeyCode -> Model -> Model
 keyUp keyCode model =
     case Key.fromCode keyCode of
         ArrowLeft ->
-            {model | player = updateAcceleration 0 model.player}
+            {model | player = updateAcceleration {x = 0, y = 0} model.player}
 
         ArrowRight ->
-            {model | player = updateAcceleration 0 model.player}
+            {model | player = updateAcceleration {x = 0, y = 0} model.player}
 
         _ ->
             model
+
+
 
 progressTime : Float -> Model -> Model
 progressTime dt model =
@@ -108,17 +124,22 @@ progressTime dt model =
 
 applySecondDerivatives: Float -> Physical t -> Physical t
 applySecondDerivatives dt physical =
-    { physical | velocity = physical.velocity + physical.accelleration * dt - (drag dt physical.velocity) }
+    let totalForce = addVecs physical.accelleration (dragForceOn physical)
+    in
+        { physical | velocity = addVecs physical.velocity (scaleVec dt totalForce)}
 
-drag : Float -> Float -> Float
-drag dt velocity =
-    velocity * dt/1000
+
+dragForceOn : Physical t -> Vector2
+dragForceOn physical =
+    let dragConst = 0.002
+    in
+        scaleVec (-1 * dragConst) physical.velocity
 
 applyFirstDerivatives: Float -> Physical t -> Physical t
 applyFirstDerivatives dt physical =
-    { physical | position = physical.position + physical.velocity * dt }
+    { physical | position = (addVecs physical.position (scaleVec dt physical.velocity)) }
 
-updateAcceleration : Float -> Physical t -> Physical t
+updateAcceleration : Vector2 -> Physical t -> Physical t
 updateAcceleration newAcceleration physical =
     { physical | accelleration = newAcceleration }
 
@@ -133,12 +154,12 @@ incrementShotsFired player =
 
 calculateLeftPercentage : Physical t -> String
 calculateLeftPercentage physical =
-    (toString (physical.position / 5000 + 50)) ++ "%"
+    (toString (physical.position.x / 5000 + 50)) ++ "%"
 
 view : Model -> Html msg
 view model =
     div
-        [style [("height", "100%"), ("width", "100%"), ("background-color", "black"), ("position", "relative")]]
+        [style [("height", "100%"), ("width", "100%"), ("background-color", "black"), ("color", "white"), ("position", "relative")]]
         [
             text (toString model),
             div
